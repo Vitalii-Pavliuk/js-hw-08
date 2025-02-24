686fac594ad94fbc796259deede919b3b7126ee7
Morello100 <114472069+Morello100@users.noreply.github.com>
2025-02-24 11:21:13 +0200

    feat/add-feedback-logic

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle'
 
+
+const formEl = document.querySelector(".feedback-form")
+const STORAGE_KEY = "feedback-form-state"
+
+formEl.addEventListener("submit",onFormSubmit)
+formEl.addEventListener("input", throttle(onInputChange,500))
+
+populateTextarea()
+ const formData = {}
+function onFormSubmit(evt){
+    evt.preventDefault();
+    evt.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
+}
+
+function onInputChange(evt){
+formData[evt.target.name] = evt.target.value
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+
+function populateTextarea(){
+    const storageRow = localStorage.getItem(STORAGE_KEY)
+        const savedMessage = JSON.parse(storageRow);
+
+    if(savedMessage){
+        formEl.elements.email.value = savedMessage.email
+        formEl.elements.message.value = savedMessage.message
+    }
+}  
